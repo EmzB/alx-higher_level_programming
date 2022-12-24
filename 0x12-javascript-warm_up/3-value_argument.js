c16a154d6c42eaaf8e6ad9401563bd91e8115e8d
Bella <emalibella45@gmail.com>
2022-12-24 19:56:47 +0300

    JS basics

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const array = process.argv.slice(2);
+if (array[0] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(array[0]);
+}
+Footer
+© 2022 GitHub, Inc.
