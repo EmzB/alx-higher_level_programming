603ed77a19971f17c24e25b0e100e5807219610c
Bella <emalibella45@gmail.com>
2023-01-16 20:47:31 +0300

    Python Input/Output

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""read file"""
+
+
+def read_file(filename=""):
+    """read file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), """reading file"""[B[B[B try catch and read file""":[B[B[B
---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""write file"""
+
+
+def write_file(filename="", text=""):
+    """"write file"""
+    with open(filename, mode="w", encoding="utf-8") as fd:
+        fd.write(text)
+    return len(text)[B[B[A[A[A[A[A[A[A[D[D[D[D[D[D[D[D[D[D[D[ing to file"""[B[B[B[B[D[D[D[D[C[C[C[opening in write mode""[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[Cfd.w[ fd.w[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+
+
+[A[A[A
+
+
+
+[A[A[A[A
+
+[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C                              fd..wr
